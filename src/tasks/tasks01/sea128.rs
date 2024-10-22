7c94e5d8fb36334abe507390e99b3059f834e04f
0xalivecow <mail@ta-lotz.de>
2024-10-22 20:22:42 +0200

    refactor: Externalise AES and SEA functions

--- src/tasks/tasks01/sea128.rs
+++ src/tasks/tasks01/sea128.rs
@@ -1,9 +1,11 @@
 use anyhow::{anyhow, Result};
 use base64::prelude::*;
-use openssl::symm::{Cipher, Crypter, Mode};
 use serde_json::Value;
 
-use crate::utils::poly::b64_2_num;
+use crate::utils::{
+    ciphers::{sea_128_decrypt, sea_128_encrypt},
+    poly::b64_2_num,
+};
 
 pub fn sea128(args: &Value) -> Result<String> {
     let key_string: String = serde_json::from_value(args["key"].clone())?;
@@ -19,42 +21,13 @@ pub fn sea128(args: &Value) -> Result<String> {
     match mode.as_str() {
         "encrypt" => {
             //eprintln!("{:?}", plaintexts);
-            let mut encrypter = Crypter::new(Cipher::aes_128_ecb(), Mode::Encrypt, &key, None)?;
-            encrypter.pad(false);
 
-            let mut ciphertext = [0; 32].to_vec();
-
-            let mut count = encrypter.update(&input, &mut ciphertext)?;
-            count += encrypter.finalize(&mut ciphertext)?;
-            ciphertext.truncate(count);
-
-            //eprintln!("{:?}", &ciphertext[..]);
-
-            let mut bytes: [u8; 16] = [0u8; 16];
-            bytes.copy_from_slice(&ciphertext);
-            let number: u128 = <u128>::from_be_bytes(bytes);
-
-            let output = BASE64_STANDARD.encode((number ^ xor_val).to_be_bytes());
+            let output = BASE64_STANDARD.encode(sea_128_encrypt(&key, &input)?);
 
             Ok(output)
         }
         "decrypt" => {
-            let mut decrypter = Crypter::new(Cipher::aes_128_ecb(), Mode::Decrypt, &key, None)?;
-            decrypter.pad(false);
-
-            let mut bytes: [u8; 16] = [0u8; 16];
-            bytes.copy_from_slice(&input);
-            let input_num: u128 = <u128>::from_be_bytes(bytes);
-
-            let input_afer_xor = (input_num ^ xor_val).to_be_bytes();
-
-            let mut plaintext = [0; 32].to_vec();
-
-            let mut count = decrypter.update(&input_afer_xor, &mut plaintext)?;
-            count += decrypter.finalize(&mut plaintext)?;
-            plaintext.truncate(count);
-
-            let output = BASE64_STANDARD.encode(plaintext);
+            let output = BASE64_STANDARD.encode(sea_128_decrypt(&key, &input)?);
 
             Ok(output)
         }
