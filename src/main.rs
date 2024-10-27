b21e60985f65116e79a27ff4999c3e68098ced51
0xalivecow <mail@ta-lotz.de>
2024-10-27 17:20:50 +0100

    chore: debug pipeline

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use anyhow::Result;
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
-    let path_to_workload = &args[1];
+    let path_to_workload = &args[2];
 
     let json = fs::read_to_string(path_to_workload).unwrap();
     let workload = kauma::utils::parse::parse_json(json)?;
