16b65b0de485f34f582eeb32f9c2ac8c649254c3
Alivecow <mail@ta-lotz.de>
2024-11-30 23:47:41 +0100

    refactor: re-enable multi threading

--- src/tasks/mod.rs
+++ src/tasks/mod.rs
@@ -234,7 +234,7 @@ pub fn task_distribute_st(testcases: &Testcases) -> Result<Responses> {
 
 pub fn task_distribute(testcases: &Testcases) -> Result<Responses> {
     let cpus = num_cpus::get();
-    if cpus > 100000 {
+    if cpus > 1 {
         task_distribute_mt(testcases)
     } else {
         task_distribute_st(testcases)
