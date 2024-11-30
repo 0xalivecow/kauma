e2ef29bfd53876afce3df0c8f37de04c6c8b2cab
Alivecow <mail@ta-lotz.de>
2024-11-30 21:16:47 +0100

    refactor: test with MT disabled

--- src/tasks/mod.rs
+++ src/tasks/mod.rs
@@ -234,7 +234,7 @@ pub fn task_distribute_st(testcases: &Testcases) -> Result<Responses> {
 
 pub fn task_distribute(testcases: &Testcases) -> Result<Responses> {
     let cpus = num_cpus::get();
-    if cpus > 1 {
+    if cpus > 1000000 {
         task_distribute_mt(testcases)
     } else {
         task_distribute_st(testcases)
